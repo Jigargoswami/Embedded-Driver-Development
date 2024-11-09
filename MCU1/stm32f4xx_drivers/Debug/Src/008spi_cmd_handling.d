4a9ac91d9f2218908c2af8637ee022e5617c8ffb
Jigar Goswami <jigargoswami116@gmail.com>
2024-11-09 16:38:56 +0530

    #1: Embedded Driver Development

---
+++ MCU1/stm32f4xx_drivers/Debug/Src/008spi_cmd_handling.d
@@ -0,0 +1,9 @@
+Src/008spi_cmd_handling.o: ../Src/008spi_cmd_handling.c \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
---
+++ MCU1/stm32f4xx_drivers/Debug/drivers/src/stm32f407xx_spi_driver.d
@@ -0,0 +1,10 @@
+drivers/src/stm32f407xx_spi_driver.o: \
+ ../drivers/src/stm32f407xx_spi_driver.c \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
+ D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
+D:/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
